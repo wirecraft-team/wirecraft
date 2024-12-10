c21d74f33ee4300bb385bf738b27fb6f335a2fa5
airo.pi_ <47398145+AiroPi@users.noreply.github.com>
2024-12-10 11:34:06 +0100

    feat: add a server interface to bind the client and the server

--- src/client/__init__.py
+++ src/client/__init__.py
@@ -3,15 +3,17 @@ import sys
 import pygame
 from pygame.locals import QUIT
 
-from server import get_value
+from .server_interface import ServerInterface
 
 
 class Game:
     def __init__(self) -> None:
         pygame.init()
+        self.server = ServerInterface()
         self.displaysurf = pygame.display.set_mode((400, 300))
 
-        pygame.display.set_caption(get_value())
+        # Example: get the money
+        self.server.get_money()
 
     def start(self):
         while True:  # main game loop
---
+++ src/client/server_interface.py
@@ -0,0 +1,18 @@
+from server import Server
+
+
+class ServerInterface:
+    """
+    Ceci est une interface faisant le lien entre le serveur et le client.
+    Elle fait partie de la partie "client".
+    Elle interagit avec le serveur comme s'il s'agissait d'une librairie, mais pourra être réimplémenté pour se
+    connecter à un serveur distant.
+
+    Dans l'état, elle n'est qu'une "duplication" de la partie "server._private", mais permettra une transition simple
+    vers un serveur distant sans trop de refactoring.
+    """
+    def __init__(self) -> None:
+        self.connection = Server()
+
+    def get_money(self) -> int:
+        return self.connection.get_money()
