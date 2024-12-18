e88316b0294ebfdf43a48a0d7e802caf7bdf94c6
airo.pi_ <47398145+AiroPi@users.noreply.github.com>
2024-12-18 11:20:21 +0100

    fix: wirecraft module not found on dev env

--- src/client/assets.py
+++ src/client/assets.py
@@ -2,6 +2,9 @@ from importlib.resources import files
 
 import pygame
 
-assets_dir = files("wirecraft").joinpath("assets")
+try:
+    assets_dir = files("wirecraft").joinpath("assets")
+except ModuleNotFoundError:  # we are in dev mode
+    assets_dir = "./assets"
 
 SWITCH_DEVICE = pygame.image.load(f"{assets_dir}/switch.png")
