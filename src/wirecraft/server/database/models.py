6c8e7e7b19d302cd69fded0dcb5f9cb82c74e0b0
LilKisss <louischtiti@outloook.fr>
2025-02-03 14:41:00 +0100

    feat: add class Level and Task

--- src/wirecraft/server/database/models.py
+++ src/wirecraft/server/database/models.py
@@ -18,3 +18,13 @@ class Device(SQLModel, table=True):
     type: str
     x: int
     y: int
+
+class Level(SQLModel, table=True):
+    id: int = Field(default=None, primary_key=True)
+    completed: bool = False
+
+class Task(SQLModel, table=True):
+    id: int = Field(default=None, primary_key=True)
+    id_level: int = Field(default=None, foreign_key="Level.id")
+    name: str
+    completed: bool = False
