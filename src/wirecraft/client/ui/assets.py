1cb486746770e700e820d37509bbf0f5a8a4480a
AiroPi <47398145+AiroPi@users.noreply.github.com>
2025-02-16 20:30:30 +0100

    feat: add svg support & comments

--- src/wirecraft/client/ui/assets.py
+++ src/wirecraft/client/ui/assets.py
@@ -1,9 +1,8 @@
-from .assets_meta import Asset, AssetsMeta
+from .assets_meta import Asset, AssetsMeta, SvgAsset
 
 
 class Assets(metaclass=AssetsMeta):
-    SWITCH_DEVICE = Asset("switch.png")
-    # SWITCH_DEVICE = pygame.image.load_sized_svg(f"switch.svg", (18910, 1660))
+    SWITCH_DEVICE = SvgAsset("switch.svg", (18910, 1660))
     PC_DEVICE = Asset("pc.png")
     CLOSE_BUTTON = Asset("close.png")
     INVENTORY_BUTTON = Asset("inventory.png")
--- src/wirecraft/client/ui/assets_meta.py
+++ src/wirecraft/client/ui/assets_meta.py
@@ -1,3 +1,11 @@
+"""
+assets_meta.py contains the "secrets" classes that make the assets.py file work "magically".
+This code is "private" in the sense that it is not meant to be used outside assets.py.
+
+This code use Metaclasses and Descriptors. These are advanced Python features that I will not explain here to avoid
+verbose explanations.
+"""
+
 from __future__ import annotations
 
 from importlib.resources import files
@@ -20,6 +28,14 @@ if not assets_dir.exists():
 
 
 class AssetsMeta(SingletonMeta):
+    """
+    A metaclass that defined the underlying class as a singleton (only one instance of the class can exist) and also
+    add a attribute to the class that will store all the assets loaders.
+
+    The assets loaders are the instances of the Asset class that are defined in the class body.
+    The class also provide a method to load all the assets.
+    """
+
     def __new__(cls, name: str, bases: tuple[Any], dct: dict[str, Any]):
         dct["__assets_loaders__"] = []
         return super().__new__(cls, name, bases, dct)
@@ -30,6 +46,14 @@ class AssetsMeta(SingletonMeta):
 
 
 class Asset:
+    """
+    An Asset is a descriptor that will lazy-load the asset on access-time.
+    That means that the asset is not loaded at definition time but when it is accessed for the first time
+
+    Alternatively, the Asset can be loaded using the Descriptor load() method, but this method is not easily accessible.
+    You should use the AssetsMeta.load_assets() method to load all the assets at once.
+    """
+
     def __init__(self, filename: str):
         self.filename = filename
         self._loaded_asset: pygame.Surface | None = None
@@ -56,3 +80,20 @@ class Asset:
             assert self._loaded_asset is not None
 
         return self._loaded_asset
+
+
+class SvgAsset(Asset):
+    """
+    Same as Asset but to load SVG files (with a specific size).
+    """
+
+    def __init__(self, filename: str, size: tuple[int, int]):
+        super().__init__(filename)
+        self.size = size
+
+    def load(self):
+        if not self.is_loaded:
+            self._loaded_asset = pygame.image.load_sized_svg(assets_dir / self.filename, self.size).convert_alpha()
+            print(f"Loaded {self.filename}")
+        else:
+            print(f"{self.filename} is already loaded !")
