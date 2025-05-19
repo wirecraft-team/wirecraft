78bfb0a3348c4c2e2c186a1ac55c5e81d9f3802c
LliKsss <100270953+LliKsss@users.noreply.github.com>
2025-05-19 09:27:53 +0200

    fix: can see task from the client but not on graphic support

--- godot/scripts/tasks.gd
+++ godot/scripts/tasks.gd
@@ -1,42 +1,21 @@
 extends Node
 
-@onready var tasks_container = get_node("/root/Control/CanvasLayer/TaskWindow/TaskContainer")
+
 
 func _init():
-	#do constructor stuff if needed
 	pass
-
 
 # Inspired by device_controller
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	call_deferred("_initialize_tasks_container")
-
-func _initialize_tasks_container():
-	tasks_container = get_node("/root/Control/CanvasLayer/TaskWindow/TaskContainer")
-	if tasks_container == null:
-		print("Error: tasks_container not found!")
-	else:
-		print("tasks_container found successfully!")
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+	$Label.text = "Bouboubou1"
 	pass
 
-func clear():
-	for child in tasks_container.get_children():
-		tasks_container.remove_child(child)
-		child.queue_free()
-
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
 
 func update_tasks(tasks: Array):
-	if tasks_container == null:
-		print("Error: tasks_container not found!++")
-		return
-	for task in tasks:
-		var task_label = Label.new()
-		task_label.text = str(task)
-		tasks_container.add_child(task_label)
-
 	print("Tasks updated in the window")
+	$Label.text = "Bouboubou2"
+	return
