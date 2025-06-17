00d5260726f8258aa2567a3868118e840088b24a
AiroPi <47398145+AiroPi@users.noreply.github.com>
2025-06-18 00:42:28 +0200

    fix: make network graph more extensible

--- python/tests/test_network.py
+++ python/tests/test_network.py
@@ -4,7 +4,7 @@ from sqlmodel import SQLModel
 from sqlmodel.ext.asyncio.session import AsyncSession
 
 from wirecraft_server import network
-from wirecraft_server.database.models import Device, LevelState
+from wirecraft_server.database.models import Cable, Device, LevelState
 
 db = "sqlite+aiosqlite:///:memory:"
 engine = create_async_engine(db)
@@ -27,9 +27,25 @@ async def test_ping(local_database):
         await session.flush()
 
         # Create a mock device
-        device = Device(id=1, name="Test Device", type="router", x=0, y=0, level_id=level_dev.id)
-        session.add(device)
+        device_a = Device(name="Test PC A", type="pc", x=0, y=0, level_id=level_dev.id, ip="192.168.0.2")
+        device_b = Device(name="Test PC B", type="pc", x=100, y=100, level_id=level_dev.id, ip="192.168.0.2")
+        session.add_all([device_a, device_b])
+        await session.flush()
+
+        session.add(
+            Cable(
+                device_id_1=device_a.id,
+                port_1=1,
+                device_id_2=device_b.id,
+                port_2=1,
+                level_id=level_dev.id,
+            )
+        )
         await session.commit()
+    print(device_a.id, device_b.id)
+
+    await network.update_devices()
+    await network.update_routing_tables()
 
     async with async_session() as session:
         # Retrieve the device and test ping
