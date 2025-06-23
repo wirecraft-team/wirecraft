099a433a321f71ab008879d8d792902bc2bcf16e
AiroPi <47398145+AiroPi@users.noreply.github.com>
2025-06-23 20:01:16 +0200

    feat: add update_device_ip handler

--- python/src/wirecraft_server/handlers/devices.py
+++ python/src/wirecraft_server/handlers/devices.py
@@ -9,6 +9,43 @@ from ..database import Device, async_session
 from ..handlers_core import Handler, event
 
 
+class DevicesHandler(Handler):
+    @event
+    async def get_level_devices(self, data: GetLevelDevicesData) -> Sequence[Device]:
+        statement = select(Device).where(Device.level_id == data.level_id)
+        async with async_session() as session:
+            result = await session.exec(statement)
+            devices = result.all()
+        return devices
+
+    @event
+    async def add_device(self, data: Device):
+        async with async_session() as session:
+            session.add(data)
+            await session.commit()
+
+    @event
+    async def update_device_position(self, data: UpdateDevicePositionData):
+        async with async_session() as session:
+            statement = select(Device).where(Device.id == data.device_id)
+            result = await session.exec(statement)
+            device = result.one()
+            device.x = data.x
+            device.y = data.y
+            await session.commit()
+        return device
+
+    @event
+    async def update_device_ip(self, data: UpdateDeviceIpData) -> Device:
+        async with async_session() as session:
+            stmt = select(Device).where(Device.id == data.device_id)
+            result = await session.exec(stmt)
+            device = result.one()
+            device.ip = data.ip
+            await session.commit()
+        return device
+
+
 class GetLevelDevicesData(BaseModel):
     """
     Payload for the get_level_devices event.
@@ -35,36 +72,11 @@ class UpdateDevicePositionData(BaseModel):
     y: int
 
 
-class DevicesHandler(Handler):
-    @event
-    async def get_level_devices(self, data: GetLevelDevicesData) -> Sequence[Device]:
-        statement = select(Device).where(Device.level_id == data.level_id)
-        async with async_session() as session:
-            result = await session.exec(statement)
-            devices = result.all()
-        return devices
+class UpdateDeviceIpData(BaseModel):
+    """
+    Payload for the update_device_ip ws method.
+    TODO(airopi): this could be merged with UpdateDevicePositionData as a PatchDeviceData
+    """
 
-    @event
-    async def add_device(self, data: Device):
-        async with async_session() as session:
-            session.add(data)
-            await session.commit()
-
-    # @event
-    # async def get_device_props(self, data: GetDevicePropsData):
-    #     async with async_session() as session:
-    #         statement = select(Device.name).where(Device.id == data.device_id)
-    #         result = await session.exec(statement)
-    #         device_name = result.one()
-    #         return device_dict[device_name]
-
-    @event
-    async def update_device_position(self, data: UpdateDevicePositionData):
-        async with async_session() as session:
-            statement = select(Device).where(Device.id == data.device_id)
-            result = await session.exec(statement)
-            device = result.one()
-            device.x = data.x
-            device.y = data.y
-            await session.commit()
-        return device
+    device_id: int
+    ip: str
