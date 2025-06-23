8d82e0db27adb91fee604cbdf1fdc1b661a79201
Syycorax <54723548+Syycorax@users.noreply.github.com>
2025-06-23 20:27:12 +0200

    fix: no, copilot please never again

--- godot/scripts/inventory.gd
+++ godot/scripts/inventory.gd
@@ -1,9 +1,8 @@
 extends Node
 
-onready var ws = get_node("../../ws")
 
 func _on_add_switch_button_pressed() -> void:
-	ws.add_device("test-switch", "switch")
+	get_node("../../ws").add_device("test-switch", "switch")
 
 func _on_add_pc_button_pressed() -> void:
-	ws.add_device("test-pc", "pc")
+	get_node("../../ws").add_device("test-pc", "pc")
