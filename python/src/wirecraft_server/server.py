364ae7045ac94d70925460f47bdfc570f269285c
AiroPi <47398145+AiroPi@users.noreply.github.com>
2025-07-07 11:05:59 +0200

    fix: use as_uri()

--- python/src/wirecraft_server/server.py
+++ python/src/wirecraft_server/server.py
@@ -129,7 +129,7 @@ class Server:
     async def _run(self):
         if ctx.database_type == "sqlite":
             path = Path(ctx.database)
-            db = f"sqlite+aiosqlite:///{path.resolve()}"
+            db = f"sqlite+aiosqlite:///{path.resolve().as_uri()}"
         elif ctx.database_type == "postgresql":
             db = f"postgresql+asyncpg://{ctx.database}"
         else:
